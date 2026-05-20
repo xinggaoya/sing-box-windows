5017a6b3f73b20ce6fc7011eb19d3d6736ceb8e7
XingGao <xinggaoya@qq.com>
2026-05-20 10:34:59 +0800

    perf: split frontend vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,51 @@ import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
+const vendorChunkGroups: Array<[string, string[]]> = [
+  [
+    'vendor-vue',
+    [
+      '/node_modules/vue/',
+      '/node_modules/@vue/',
+      '/node_modules/vue-router/',
+      '/node_modules/vue-i18n/',
+      '/node_modules/pinia/',
+      '/node_modules/pinia-plugin-persistedstate/',
+      '/node_modules/@vueuse/',
+      '/node_modules/mitt/',
+    ],
+  ],
+  [
+    'vendor-naive-ui',
+    ['/node_modules/naive-ui/'],
+  ],
+  [
+    'vendor-ui-utils',
+    [
+      '/node_modules/vueuc/',
+      '/node_modules/vooks/',
+      '/node_modules/vdirs/',
+      '/node_modules/css-render/',
+      '/node_modules/@css-render/',
+      '/node_modules/treemate/',
+      '/node_modules/evtd/',
+      '/node_modules/async-validator/',
+      '/node_modules/date-fns/',
+    ],
+  ],
+  ['vendor-icons', ['/node_modules/@vicons/', '/node_modules/ionicons/']],
+  ['vendor-tauri', ['/node_modules/@tauri-apps/']],
+]
+
+const manualChunks = (id: string) => {
+  const normalizedId = id.replace(/\\/g, '/')
+  if (!normalizedId.includes('/node_modules/')) return undefined
+
+  return vendorChunkGroups.find(([, packages]) =>
+    packages.some((packagePath) => normalizedId.includes(packagePath)),
+  )?.[0]
+}
+
 // https://vite.dev/config/
 export default defineConfig(({ mode: _mode }) => ({
   plugins: [
@@ -26,5 +71,12 @@ export default defineConfig(({ mode: _mode }) => ({
     host: '0.0.0.0',
     port: 6221,
     strictPort: true,
-  }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks,
+      },
+    },
+  },
 }))
